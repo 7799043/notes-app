b26ba1a48b3b376dd6cd6473994d89daa87aefa9
7799043 <leoniak.maciek1@gmail.com>
2023-05-22 20:25:43 +0200

    .

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/Homepage', (req, res) => {
-  res.send('Homepage!')
+app.get('/', (req, res) => {
+    res.sendFile("pages/index.html", {root: __dirname})
 })
 
 app.get('/login', (req, res) => {
-    res.send('login!')
-  })
+    res.sendFile("pages/login.html", {root: __dirname})
+})
 
-  app.get('/singup', (req, res) => {
-    res.send('singup!')
-  })
+app.get('/singup', (req, res) => {
+    res.sendFile("pages/singup.html", {root: __dirname})
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
