e1a32728704daa6f8fbd3db5558d4249e5e66c37
7799043 <leoniak.maciek1@gmail.com>
2023-05-23 18:03:01 +0200

    .

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const mongoose = require('mongoose')
 const Note = require('./models/Note')
 const User = require('./models/User')
 const app = express()
-app.use(express.json({extended:true }))
+app.use(express.json({ extended: true }))
 app.use(express.urlencoded())
 const port = 3000
 
 mongoose.connect('mongodb://leoniakmaciek1:rkqMA30u@ac-ig4sdoa-shard-00-02.yamms2s.mongodb.net:27017/?tlsAllowInvalidHostnames=true&tlsAllowInvalidCertificates=true&tls=true&authMechanism=DEFAULT')
-  .then(() => {
-    console.log('Successfully Connected');
-  })
-  .catch(() => {
-    console.error('Error');
-  }); 
+    .then(() => {
+        console.log('Successfully Connected');
+    })
+    .catch(() => {
+        console.error('Error');
+    });
 
 // Endopoints to server the
 app.get('/', (req, res) => {
@@ -37,10 +37,10 @@ app.post('/getnotes', (req, res) => {
 app.post('/login', (req, res) => {
     let user = User.find(req.body)
     console.log(user)
-    if(!user){
-        res.status(200).json({success: false, massage: "No user found"})
-    }else{
-        res.status(200).json({success: true, user: {email: user.email}, massage: "User found"})
+    if (!user) {
+        res.status(200).json({ success: false, massage: "No user found" })
+    } else {
+        res.status(200).json({ success: true, user: { email: user.email }, massage: "User found" })
     }
 
     res.sendFile("pages/signup.html", { root: __dirname })
@@ -49,8 +49,8 @@ app.post('/login', (req, res) => {
 app.post('/signup', async (req, res) => {
     const { userToken } = req.body
     console.log(req.body)
-    let user= await User.create(req.body)
-    res.status(200).json({success:true, user: user})
+    let user = await User.create(req.body)
+    res.status(200).json({ success: true, user: user })
 })
 
 app.post('/addnote', (req, res) => {
