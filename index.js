beb705ce0e99117d2aee7acf23b55ce39cb8b0d7
7799043 <leoniak.maciek1@gmail.com>
2023-05-23 08:16:59 +0200

    .

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded())
 const port = 3000
-
+// Endopoints to server the
 app.get('/', (req, res) => {
     res.sendFile("pages/index.html", {root: __dirname})
 })
@@ -11,6 +13,32 @@ app.get('/login', (req, res) => {
 })
 
 app.get('/singup', (req, res) => {
+    res.sendFile("pages/singup.html", {root: __dirname})
+})
+
+// Endopoints for APIs
+app.get('/getnotes', (req, res) => {
+    const {userToken } = req.body
+    res.sendFile("pages/singup.html", {root: __dirname})
+})
+
+app.get('/login', (req, res) => {
+    const {userToken } = req.body
+    res.sendFile("pages/singup.html", {root: __dirname})
+})
+
+app.get('/singup', (req, res) => {
+    const {userToken } = req.body
+    res.sendFile("pages/singup.html", {root: __dirname})
+})
+
+app.get('/addnote', (req, res) => {
+    const {userToken } = req.body
+    res.sendFile("pages/singup.html", {root: __dirname})
+})
+
+app.get('/deletenote', (req, res) => {
+    const {userToken } = req.body
     res.sendFile("pages/singup.html", {root: __dirname})
 })
 
