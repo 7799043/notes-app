06010811760ecad4477384458f9f779a45299c4b
7799043 <leoniak.maciek1@gmail.com>
2023-05-25 08:34:52 +0200

    .

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.post('/signup', async (req, res) => {
 app.post('/addnote', async (req, res) => {
     const { userToken } = req.body
     let note = await Note.create(req.body)
-    res.status(200).json({ success: true, note})     //????? note: note
+    res.status(200).json({ success: true, note})     
 })
 
 app.post('/deletenote', (req, res) => {
