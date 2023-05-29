7b8ac29cfdea060a8c3e76fabccd9a69039259c8
7799043 <leoniak.maciek1@gmail.com>
2023-05-29 18:58:02 +0200

    .

--- models/Note.js
+++ models/Note.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
 
 const noteSchema = new mongoose.Schema({
+    noteId: {
+        type: String,
+        default: () => new mongoose.Types.ObjectId().toString(), 
+        unique: true,
+      },
     email: { type: String, requred: true },
     title: { type: String, requred: true }, 
     desc: { type: String, requred: true },
